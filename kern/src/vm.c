3c8229de86891a718193f071a3c0444c76d33ef4
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2025-07-26 12:35:28 -0700

    free_page

--- kern/src/vm.c
+++ kern/src/vm.c
@@ -1,6 +1,28 @@
+#include <kern/emerg.h>
+#include <kern/platform.h>
 #include <kern/vm.h>
 
-extern char __phy_load_end[];
+extern u8 __phy_load_end[];
+
+struct freelist_page {
+	struct freelist_page *next;
+} *freelist = NULL;
+
+static void free_page(void *paddr) {
+	struct freelist_page *fp = paddr;
+	fp->next = freelist;
+	freelist = fp;
+}
+
+static void freerange(u8 *start_paddr, usize end_paddr) {
+	assert((usize)start_paddr % PAGESIZE == 0);
+
+	while ((usize)start_paddr < end_paddr) {
+		free_page(start_paddr);
+		start_paddr += PAGESIZE;
+	}
+}
 
 void vm_init(void) {
+	freerange(__phy_load_end, PHYSTOP);
 }
--- shared/types.h
+++ shared/types.h
@@ -4,6 +4,7 @@
 #define NULL ((void *)0)
 
 /* As per ARM Architecture Procedure Call Standard, section 8.1. */
+typedef unsigned char u8;
 typedef unsigned int usize;
 
 #endif
